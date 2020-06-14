0e5cd9b124684c5dba7ec6e050029ac51c4103f8
Stephanie Djajadi <sdjajadi@berkeley.edu>
2020-06-14 15:56:19 -0700

    update file name

--- 2-analysis/0-run-analysis.sh
+++ 2-analysis/0-run-analysis.sh
@@ -4,4 +4,4 @@ R CMD BATCH 1-obtain-priors-state.R
 R CMD BATCH 2-est-expected-cases-state.R 
 R CMD BATCH 3-est-expected-cases-state-perf-testing.R
 R CMD BATCH 4-obtain-testing-protocols.R
-R CMD BATCH 4-summarize-results.R 
+R CMD BATCH 5-summarize-results.R 
