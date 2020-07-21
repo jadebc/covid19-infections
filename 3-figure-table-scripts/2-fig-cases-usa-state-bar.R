9f59bb6a0d2892699244dccbc1ffc565161c444a
Stephanie Djajadi <sdjajadi@berkeley.edu>
2020-07-21 02:42:03 -0700

    updated plot to only be estimated infections

--- 3-figure-table-scripts/2-fig-cases-usa-state-bar.R
+++ 3-figure-table-scripts/2-fig-cases-usa-state-bar.R
@@ -53,14 +53,14 @@ perdiff_solo_plot = ggplot(bar_data_perdiff, aes(x = statename_f, y = factor_dif
   geom_linerange(aes(ymin = factor_diff_lb, ymax = factor_diff_ub)) + 
   xlab("") +
   scale_fill_manual(values = gnbu_colors) +
-  scale_y_continuous(labels = seq(0,50,5), breaks = seq(0,50,5)) +
+  scale_y_continuous(labels = seq(0,80,5), breaks = seq(0,80,5)) +
   ylab("Ratio of estimated infections vs. confirmed cases") +
   coord_flip() +
   theme_minimal() +
-  theme(legend.position = "none") 
+  theme(axis.text = element_text(size=10.5)) +
+  theme(legend.position = "none")
 
 perdiff_plot = perdiff_solo_plot +  ggtitle("B) Ratio of estimated infections vs. confirmed cases")
-
 
 perdiff_plot_int <- ggplotly(perdiff_plot, tooltip="text") %>% 
   style(hoverlabel = list(align = "left")) %>% 
@@ -94,39 +94,32 @@ perdiff_solo_plot_int <- ggplotly(perdiff_solo_plot, tooltip="text") %>%
 perdiff_plot_int
 
 ############################################################
-# Bar chart of absolute counts (Panel A)
+# Bar chart of estimated counts (Panel A)
 ############################################################
+
 bar_data =  bar_data_perdiff %>%
-  mutate(obs = positive, 
-         exp = estimated_cases,
-         obs_perpop = positive/population *1000,
+  mutate(exp = estimated_cases,
          exp_perpop = estimated_cases/population*1000,
          exp_perpop_lb = estimated_cases_lb / population*1000,
          exp_perpop_ub = estimated_cases_ub / population*1000) %>%
-  mutate(statename_f = fct_reorder(statename, obs_perpop, max)) %>% 
-  dplyr::select(statename, statename_f, obs_perpop,  exp_perpop,
+  dplyr::select(statename, statename_f,  exp_perpop,
                 exp_perpop_lb, exp_perpop_ub) %>%
   melt(id.vars = c("statename", "statename_f", "exp_perpop_lb","exp_perpop_ub")) %>%
   mutate(variable = as.character(variable)) %>%
-  mutate(variable_f = as.factor(ifelse(variable == "obs_perpop", "Confirmed cases", 
-                                       "Estimated infections"))) %>%
-  mutate(variable_f = factor(variable_f, levels = c("Confirmed cases", "Estimated infections"))) %>%
-  mutate(exp_perpop_lb = ifelse(variable=="obs_perpop", NA, exp_perpop_lb),
-         exp_perpop_ub = ifelse(variable=="obs_perpop", NA, exp_perpop_ub))
+  mutate(variable_f = as.factor("Estimated infections")) %>%
+  mutate(variable_f = factor(variable_f, "Estimated infections")) %>%
+  mutate(exp_perpop_lb = exp_perpop_lb, exp_perpop_ub = exp_perpop_ub)
+
 
 # create label for interactive plot
 bar_labels = bar_data %>% dplyr::select(statename) %>%
-  left_join(bar_data %>% filter(variable == "obs_perpop") %>%
-              dplyr::select(statename, value), by = "statename") %>%
   left_join(bar_data %>% filter(variable == "exp_perpop") %>%
               dplyr::select(statename, value), by = "statename") %>%
   left_join(bar_data %>% filter(variable == "exp_perpop") %>% 
               dplyr::select(statename, exp_perpop_lb, exp_perpop_ub), by = "statename") %>%
   mutate(label = paste0("<b>", statename, "</b>\n",
-                        "Confirmed Cases: ", 
-                        format(value.x, big.mark=",", digits=1, scientific=F, trim = TRUE), "\n",
                         "Estimated Cases: ", 
-                        format(value.y, big.mark=",", digits=1, scientific=F, trim = TRUE), 
+                        format(value, big.mark=",", digits=1, scientific=F, trim = TRUE), 
                         " (", format(exp_perpop_lb, big.mark=",", digits=1, scientific=F, trim = TRUE), 
                         ", ", format(exp_perpop_ub, big.mark=",", digits=1, scientific=F, trim = TRUE), ")"
                         )) %>%
@@ -136,26 +129,26 @@ bar_data = bar_data %>%
   left_join(bar_labels, by = "statename")
 
 
-bar_data$variable_f = factor(bar_data$variable_f, levels= c("Confirmed cases", "Estimated infections"))
+bar_data$variable_f = factor(bar_data$variable_f, "Estimated infections")
 
 
 ############################################################
 # natural scale plot
 ############################################################
 
-nat_bar_solo = ggplot(bar_data, 
+nat_bar_solo = ggplot(bar_data,
                       aes(x = statename_f, y = value , fill = variable_f, text = label)) +
-  
   geom_bar(stat="identity", position=position_dodge(width=0.8), width=0.8) +
-  geom_linerange(aes(ymin = exp_perpop_lb, ymax = exp_perpop_ub), 
+  geom_linerange(aes(ymin = exp_perpop_lb, ymax = exp_perpop_ub),
                  position=position_dodge(width=0.8)) +
 
-  scale_fill_manual("", values = c("#E5BC4D","#858585"))  + 
+  scale_fill_manual("", values = "#858585")  +
   
-  theme_minimal() + 
-  xlab("") + 
-  ylab("Cumulative COVID-19 infections per 1,000") +
-  theme(legend.position = "bottom")+
+  theme_minimal() +
+  theme(axis.text.y = element_text(size=10.5)) +
+  xlab("") +
+  ylab("Cumulative estimated COVID-19 infections per 1,000") +
+  theme(legend.position = "none")+
   coord_flip()+
   
   scale_y_continuous(breaks = seq(0,150,10),
@@ -164,11 +157,12 @@ nat_bar_solo = ggplot(bar_data,
 
 nat_bar_solo
 
-nat_bar = nat_bar_solo + ggtitle("A) Cumulative COVID-19 infections per 1,000") 
+nat_bar = nat_bar_solo + ggtitle("A) Cumulative estimated COVID-19 infections per 1,000") 
 
 
-bar_data_forint = bar_data %>% mutate(variable_f = factor(bar_data$variable_f, 
-                                                          levels= c("Estimated infections", "Confirmed cases")))
+bar_data_forint = bar_data %>% 
+  mutate(variable_f = factor(bar_data$variable_f, 
+                             levels= c("Estimated infections")))
 
 nat_bar_solo_forint = ggplot(bar_data_forint, 
                              aes(x = statename_f, y = value , fill = variable_f, text = label)) +
@@ -180,8 +174,8 @@ nat_bar_solo_forint = ggplot(bar_data_forint,
   
   theme_minimal() + 
   xlab("") + 
-  ylab("Cumulative COVID-19 infections per 1,000") +
-  theme(legend.position = "bottom")+
+  ylab("Cumulative estimated COVID-19 infections per 1,000") +
+  theme(legend.position = "none")+
   coord_flip()
 
 nat_bar_forint = nat_bar_solo_forint +  ggtitle("A) Cumulative case count") 
@@ -190,7 +184,7 @@ nat_bar_int =  ggplotly(nat_bar_forint + scale_fill_manual("", values = c("#8585
   style(hoverlabel = list(align = "left")) %>% 
   layout(barmode = 'overlay',
          annotations = list(
-           text = "A) Cumulative COVID-19 infections per 1,000",
+           text = "A) Cumulative estimated COVID-19 infections per 1,000",
            xref = "paper",
            yref = "paper",
            yanchor = "bottom",
@@ -206,7 +200,7 @@ nat_bar_solo_int =  ggplotly(nat_bar_solo_forint + scale_fill_manual("", values 
   style(hoverlabel = list(align = "left")) %>% 
   layout(barmode = 'overlay',
          annotations = list(
-           text = "Cumulative COVID-19 infections per 1,000",
+           text = "Cumulative estimated COVID-19 infections per 1,000",
            xref = "paper",
            yref = "paper",
            yanchor = "bottom",
@@ -229,13 +223,10 @@ ggsave(perdiff_solo_plot, filename = paste0(plot_path, "fig-usa-state-cases-bar-
 
 
 ### save combined plots
-blank = grid.rect(gp=gpar(col="white"))
-panel_2 = grid.arrange(perdiff_plot, blank, nrow = 2, heights = c(17, 1.4))
-
-bar_plot = grid.arrange(nat_bar, panel_2, ncol = 2)
+bar_plot = grid.arrange(nat_bar, perdiff_plot, ncol = 2)
 
 ggsave(bar_plot, filename = paste0(plot_path, "fig-usa-state-cases-bar.png"),
-       width=12, height=7)
+       width=12, height=8)
 
 ### save separate interactive plots
 saveWidget(nat_bar_solo_int, paste0(plot_path, "fig-usa-state-cases-bar-obs.html"), selfcontained = T)
